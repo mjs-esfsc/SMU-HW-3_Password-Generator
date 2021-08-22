6ffa44c8ba2bbb7126a3fc0ca93ef7c09e00ece4
esfsc-90 <67880570+mjs-esfsc@users.noreply.github.com>
2021-08-22 16:06:52 -0500

    got display area working

--- jss/script.js
+++ jss/script.js
@@ -1,18 +1,20 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+function generatePassword() {
 
 
-function generatePassword(){
+  var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numberChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialChar = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  
   // WHEN prompted for the length of the password
   var length = prompt("Enter password length")
   // THEN I choose a length of at least 8 characters and no more than 128 characters
-  console.log(parseInt(length))
-  console.log(typeof parseInt(length))
+  //console.log(parseInt(length))
+  //console.log(typeof parseInt(length))
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
   // WHEN I answer each prompt
@@ -30,7 +32,7 @@ function writePassword() {
   // THEN the password is either displayed in an alert or written to the page
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  alert(password);
+ 
 
 }
 
