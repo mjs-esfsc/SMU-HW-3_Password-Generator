396f1ad8f71c798ab35eb2193fd5cea29081baa9
esfsc-90 <67880570+mjs-esfsc@users.noreply.github.com>
2021-08-22 23:47:42 -0500

    file cleanup and preliminary submission

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SMU-HW-3_Password-Generator
+
+Assignment is to create Javascript password generator.
+
+We will use query selectors to assign click action to a button then create a password of length of 8 to 128 characters.
+
+Logic will test if th euser length  onput is an integer within 8 to 128 characters.
+
+We will ask user what types and cases may be included.
+
+We will generate a password and display the alert.
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 // Assignment Code
-console.log("well shit");
 var container = document.querySelector(".container");
 var generateBtn = document.querySelector("#generate");
 
@@ -14,15 +13,14 @@ function generatePassword() {
   // WHEN prompted for the length of the password
   var passLength = prompt("Enter password length from 8 to 128 characters")
   // THEN I choose a length of at least 8 characters and no more than 128 characters
-  console.log(parseInt(length))
-  console.log(typeof parseInt(length))
+  console.log(parseInt(passLength))
+  console.log(typeof parseInt(passLength))
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
   // WHEN I answer each prompt
   // THEN my input should be validated and at least one character type should be selected
   // WHEN all prompts are answered
   // THEN a password is generated that matches the selected criteria
-  console.log("test");
   return "test"
 }
 
@@ -33,7 +31,7 @@ function writePassword() {
   // THEN the password is either displayed in an alert or written to the page
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  console.log(password);
+  alert("Password is "+passwordText.value)
 }
 
 
